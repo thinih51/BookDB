550296edbab3485de1344d5d55f6f5e6d68bfa5b
thinih51 <shedathi@students.zhaw.ch>
2024-12-11 14:23:02 +0100

    ready for deployment on netlify

---
+++ src/routes/books/[id]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    let { data } = $props();
+</script>
+
+<div class="container mt-4">
+    <a href="/books">Back</a>
+    <h1>{data.book.title}</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <img src={data.book.cover} alt={data.book.title} class="img-fluid" />
+        </div>
+        <div class="col-md-8">
+            <p>Jahr: {data.book.year}</p>
+            <p>Dauer: {data.book.pages}</p>
+            <p>Cast:</p>
+            <ul>
+                {#each data.book.authors as author}
+                    <li>{author}</li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+</div>
